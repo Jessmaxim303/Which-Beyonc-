2d9731e9a83fd2849ca044bc4c67b8b37ca57b0f
Jesse Maxim <jessmaxim303@gmail.com>
2019-09-16 20:32:43 -0600

    Completed iteration-1c cards match, delete and matches increase

--- card.js
+++ card.js
@@ -4,10 +4,13 @@ class Card {
 		this.matchInfo = cards.matchInfo;
 		this.matched = cards.matched;
 		this.flipped = cards.flipped;
+		this.dataName = cards.dataName;
 	}
 
 	match() {
+		document.querySelector(`.game__card[data-name="${this.dataName}"]`).classList.add('js__display--none');
 	}
+
 	
 };
 
