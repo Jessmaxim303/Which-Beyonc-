d3c079fd4d912ec37c7e8b98329deb003551996a
Jesse Maxim <jessmaxim303@gmail.com>
2019-09-17 22:19:34 -0600

    Commit to push

--- deck.js
+++ deck.js
@@ -7,15 +7,20 @@ class Deck {
 	}
 
 	shuffle() {
-
 	}
 
 	checkSelectedCards() {
-
+		if (this.selectedCards[0].matchInfo === this.selectedCards[1].matchInfo && this.selectedCards[0].dataName !== this.selectedCards[1].dataName) {
+			this.selectedCards[0].matched = true;
+			this.selectedCards[1].matched = true;
+			this.matches++;
+		}
 	}
 
 	moveToMatched() {
-
+		for (var i = 0; i < this.selectedCards.length; i++) {
+		this.matchedCards.push(this.selectedCards[i]);
+		}
 	}
 
 
